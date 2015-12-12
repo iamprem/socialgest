0ac523e5beb9026f0a2d76649776b3decca894b4
iamprem <mprem@outlook.com>
2015-12-12 00:45:51 -0500

    401 - 800 DTW

--- knn_dtw.m
+++ knn_dtw.m
@@ -1,5 +1,5 @@
-function [ testY, valtestY, trainX, trainY, valX, valY, testX ] = knn_dwt( trainData, trainAction, testData )
-%KNN_DWT KNN with Dynamic Time wrapping distance as distance measurement
+function [ testY, valtestY, trainX, trainY, valX, valY, testX ] = knn_dtw( trainData, trainAction, testData, k )
+%KNN_DTW K-NN with Dynamic Time wrapping distance as distance measurement
     
 % Clean the noisy data by removing them
 idx = zeroindices(trainData);
@@ -19,6 +19,7 @@ testX = testData;
 
 
 testY = zeros(1, size(testX,2));
+dtw_dist = zeros(size(testX,2), size(trainX,2)); %Rows represent each testX, Column correspond to each trainX
 valtestY = [];%zeros(1, size(valX,2));
     
 %train with trainX, trainY
@@ -38,16 +39,14 @@ valtestY = [];%zeros(1, size(valX,2));
 % t1 = toc();
 % fprintf('Validation Running time=%f\n',t1);
 
-% evaluate(valY, valtestY)
-
 % Actual testing
 tic();
 for i = 801:1000%size(testX,2)
-    dtw_dist = zeros(1, size(trainX,2));
+    
     x = testX{1,i};
     parfor j = 1:size(trainX,2)
         z = trainX{1,j};
-        dtw_dist(j) = dtw(z, x, 50);
+        dtw_dist(i,j) = dtw(z, x, 50);
     end
     [M,I] = min(dtw_dist);
     testY(i) = trainY(I);
@@ -57,5 +56,6 @@ t2 = toc();
 
 fprintf('Testing Running time=%f\n',t2);
 
-end
+save('testX_dtwdist.mat','dtw_dist');
 
+end
